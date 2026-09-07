2fc5450cd85fa81b6140336f54d7f91de2051952
NAGAVELLI SAI TEJA <mail2saitejanagavelli75@gmail.com>
2026-09-07 16:20:01 +0530

    Update Class with multiple objects.py

--- Class with multiple objects.py
+++ Class with multiple objects.py
@@ -1,11 +1,11 @@
 class student:
-    def __init__(self, name, ):
-        self.name=name
-        self.age=age
-        def read(self):
-            print(self.name)
-            print(self.age)
-            s1=student("kprit",20)
-            s1.read()
-            s2=student("abc",21)
-            s2.read()
+def __init__(self, name, ):
+self.name=name
+self.age=age
+def read(self):
+print(self.name)
+print(self.age)
+s1=student("kprit",20)
+s1.read()
+s2=student("abc",21)
+s2.read()
